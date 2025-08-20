57477f2d1af2d5ef445aa5d5b8c495a79b8bf0b3
Sudeeptapaul <sudeeptapaul005@gmail.com>
2025-08-20 19:41:46 +0530

    Update urls.py

--- diabetes/urls.py
+++ diabetes/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from takecare.views import home
+from takecare.views import home,index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('takecare.urls')),
-    path('',home,name="home")
+    path('',index,name="index"),
+    path('index/',home,name="home")
 ]
+
